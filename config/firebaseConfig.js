83a28e6cf506d94ddc748f89c90cb08738cf5584
mmanka1 <33776177+mmanka1@users.noreply.github.com>
2021-04-14 16:23:54 -0400

    Gitignore updated

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,12 +1,3 @@
-export default {
-    firebaseConfig: {
-        apiKey: "AIzaSyCaMwYff_-HW82QneFIU1DWZ5VKNL3jq9U",
-        authDomain: "nutrition-analyzer-proj.firebaseapp.com",
-        projectId: "nutrition-analyzer-proj",
-        storageBucket: "nutrition-analyzer-proj.appspot.com",
-        messagingSenderId: "637581729734",
-        appId: "1:637581729734:web:d7087ab2f812f9a97fdcc7",
-        measurementId: "G-JPZ0V6MZ0V"
-  }
-}
+import firebaseConfig from './firebase';
+export default firebaseConfig;
 
